e2452ff2d28c79d16856ddcda9938c4d84f2fe4f
viniciusOP <viniciusopassos@hotmail.com>
2022-10-26 17:27:56 -0300

    feat: estrutura de repetição p/ exibir todas as placas da lista veiculos

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -46,8 +46,11 @@ namespace DesafioFundamentos.Models
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                
+                foreach (string placa in veiculos)
+                {
+                    Console.WriteLine(placa);
+                }
             }
             else
             {
